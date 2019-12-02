349de7ec96bd3b9f9a28e03f1df2135abbbd7a27
Michał <michaljanpatyk@gmail.com>
2019-12-02 14:05:42 +0100

    Add logic file with todo's
    Signed-off-by: Michał <michaljanpatyk@gmail.com>

---
+++ src/logic.cpp
@@ -0,0 +1,12 @@
+#include "logic.h"
+
+//todo zamknięto drzwi gdy temperatura poniżej 100 - zapytaj czy rozpocząć rozpalanie
+
+//todo gdy po rozpaleniu (czas rozpalania) temperatura spadnie poniżej zadana - 20 - informuj o potrzebie dołożenia paliwa
+
+//todo gdy temperatura w pomieszczeniu zgodna z zadaną - włącz wentylator; jeśli spada poniżej zadanej wyłącz
+
+//todo gdy długo po rozpalaniu i paliwo wypalone zamknij przepustnicę
+
+//todo alarmuj gdy temperatura przekroczona
+
