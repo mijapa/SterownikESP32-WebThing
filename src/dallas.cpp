f6849852eedd6e2b9e643397779b4a7eb337b9b1
Michał <michaljanpatyk@gmail.com>
2019-12-27 20:22:15 +0100

    Correct dallas routines
    Signed-off-by: Michał <michaljanpatyk@gmail.com>

--- src/dallas.cpp
+++ src/dallas.cpp
@@ -1,13 +1,12 @@
 #include "dallas.h"
-// Include the libraries we need
 #include <OneWire.h>
 #include <DallasTemperature.h>
 #include <HardwareSerial.h>
 
-// Data wire is plugged into port 2 on the Arduino
 #define ONE_WIRE_BUS 3
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
+
 OneWire oneWire(ONE_WIRE_BUS);
 
 // Pass our oneWire reference to Dallas Temperature.
@@ -17,44 +16,39 @@ DallasTemperature sensors(&oneWire);
  * The setup function. We only start the sensors here
  */
 uint8_t findDevices(OneWire ow, int pin) {
+    Serial.println("Looking for OneWire devices");
     uint8_t address[8];
     uint8_t count = 0;
 
-
-    if (ow.search(address)) {
-        Serial.print("\nuint8_t pin");
-        Serial.print(pin, DEC);
-        Serial.println("[][8] = {");
-        do {
-            count++;
-            Serial.println("  {");
-            for (uint8_t i = 0; i < 8; i++) {
-                Serial.print("0x");
-                if (address[i] < 0x10) Serial.print("0");
-                Serial.print(address[i], HEX);
-                if (i < 7) Serial.print(", ");
-            }
-            Serial.println("  },");
-        } while (ow.search(address));
-
-        Serial.println("};");
-        Serial.print("// nr devices found: ");
-        Serial.println(count);
+    if (!ow.search(address)) {
+        Serial.println(" No more addresses.");
+        Serial.println();
+        ow.reset_search();
+        delay(250);
+    } else {
+        Serial.print(" ROM =");
+        for (int i = 0; i < 8; i++) {
+            Serial.write(' ');
+            Serial.print(address[i], HEX);
+        }
     }
-
     return count;
 }
 
 void setupDallas() {
     // start serial port
     Serial.println("Dallas Temperature IC Control Library Demo");
-    findDevices(oneWire, ONE_WIRE_BUS);
+
+//    pinMode(ONE_WIRE_BUS, INPUT_PULLUP);
 
     // Start up the library
     sensors.begin();
 }
 
 void loopDallas() {
+//    Serial.println(digitalRead(ONE_WIRE_BUS));
+//    findDevices(oneWire, ONE_WIRE_BUS);
+//    delay(2000);
     // call sensors.requestTemperatures() to issue a global temperature
     // request to all devices on the bus
     Serial.print("Requesting temperatures...");
