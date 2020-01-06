1d57755425d6e67785e6a768faebcb5e6a2d1ceb
Michał <michaljanpatyk@gmail.com>
2020-01-06 20:38:01 +0100

    Clean dht file

--- src/dht.cpp
+++ src/dht.cpp
@@ -1,9 +1,7 @@
 #include <DHTesp.h>
-#include <Ticker.h>
 #include "dht.h"
 
 DHTesp dht;
-Ticker sampler;
 
 void setupDHT() {
     dht.setup(DHTPIN, DHTesp::DHT22);
@@ -21,21 +19,6 @@ int readDhtHeatIndex(double *heatIndex) {
     }
     Serial.println("FAILED TO READ DHT HEAT INDEX!!");
     return 0;
-}
-
-void readDHT(float *temperature, float *humidity) {
-    Serial.println("Reading temperature and humidity...");
-    *humidity = dht.getHumidity();
-    *temperature = dht.getTemperature();
-    Serial.println("Status\tHumidity (%)\tTemperature (C)\tHeatIndex (C)");
-    Serial.print(dht.getStatusString());
-    Serial.print("\t");
-    Serial.print(*humidity, 1);
-    Serial.print("\t\t");
-    Serial.print(*temperature, 1);
-    Serial.print("\t\t");
-    Serial.print(dht.computeHeatIndex(*temperature, *humidity, false), 1);
-    Serial.print("\n");
 }
 
 double readDHTtemp() {
